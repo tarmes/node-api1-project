87a690417ee5995b3841666c3f7c2a4cd4b6c84f
tarmes <traarmes@gmail.com>
2020-10-26 17:09:25 -0400

    server set up with initial data

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world NOW')
+const express = require('express')
+const generate = require('shortid').generate
+
+const app = express()
+app.use(express.json())
+
+const PORT = 5000
+
+let users = [
+   { 
+      id: generate(), 
+      name: 'Trevor Armes', 
+      bio: 'Indiana raised, worldwide livin' 
+   }
+]
